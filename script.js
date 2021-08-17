19eb141980661e01f67dfb57815001e3f067b9e3
chillyweather <d.dzmitryeu@gmail.com>
2021-08-17 20:29:33 +0300

    feat: main form logic done

--- script.js
+++ script.js
@@ -1,11 +1,39 @@
-const heartToggle = document.querySelector('.element__like-icon');
+const popupForm = document.querySelector('.popup');
 
-function toggleHeart() {
-  if (heartToggle.src = './images/svg/Heart.svg') {
-    heartToggle.setAttribute('src', './images/svg/Heart_black.svg');
-  } else {
-    heartToggle.setAttribute('src', './images/svg/Heart.svg');
-  }
+const popupCloseButton = document.querySelector('.popup__close-button');
+
+const profileEditButton = document.querySelector('.profile__edit-button');
+
+const submitProfileInfoButton = document.querySelector('.popup__submit-button');
+
+// form inputs
+const inputName = document.querySelector('.popup__submit-text_name');
+const inputAbout = document.querySelector('.popup__submit-text_about');
+
+// text in profile
+const profileName = document.querySelector('.profile__name');
+const profileAbout = document.querySelector('.profile__about');
+
+// toggle popup window function
+
+function togglePopup() {
+  popupForm.classList.toggle('popup_opened');
+  inputName.value = profileName.textContent;
+  inputAbout.value = profileAbout.textContent;
+
 }
 
-heartToggle.addEventListener('click', toggleHeart);
+// update profile info function
+
+function submitProfileInfo(event) {
+  event.preventDefault();
+  profileName.textContent = inputName.value;
+  profileAbout.textContent = inputAbout.value;
+  togglePopup();
+}
+
+profileEditButton.addEventListener('click', togglePopup);
+
+popupCloseButton.addEventListener('click', togglePopup);
+
+submitProfileInfoButton.addEventListener('click', submitProfileInfo);
