96b4033de06987a71cdb571c583ff8a4a839d786
chillyweather <d.dzmitryeu@gmail.com>
2021-08-25 17:16:20 +0300

    project_5 start

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,34 @@ const popupForm = document.querySelector('.popup');
 
 const popupCloseButton = document.querySelector('.popup__close-button');
 
+// initial cards
+const initialCards = [
+  {
+    name: 'Yosemite Valley',
+    link: 'https://code.s3.yandex.net/web-code/yosemite.jpg',
+  },
+  {
+    name: 'Lake Louise',
+    link: 'https://code.s3.yandex.net/web-code/lake-louise.jpg',
+  },
+  {
+    name: 'Bald Mountains',
+    link: 'https://code.s3.yandex.net/web-code/bald-mountains.jpg',
+  },
+  {
+    name: 'Latemar',
+    link: 'https://code.s3.yandex.net/web-code/latemar.jpg',
+  },
+  {
+    name: 'Vanoise National Park',
+    link: 'https://code.s3.yandex.net/web-code/vanoise.jpg',
+  },
+  {
+    name: 'Lago di Braies',
+    link: 'https://code.s3.yandex.net/web-code/lago.jpg',
+  },
+];
+
 // form inputs
 const nameInput = document.querySelector('.popup__submit-text_content_name');
 const jobInput = document.querySelector('.popup__submit-text_content_about');
