87f620f0cbe6ada913a1d14a38a924a7d365ad52
chillyweather <d.dzmitryeu@gmail.com>
2021-09-06 16:53:40 +0300

    feat: empty validation.js is finished

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,8 +1,72 @@
 const input = {
-  formSelector: '.popup__form',
-  inputSelector: '.popup__input',
-  submitButtonSelector: '.popup__button',
-  inactiveButtonClass: 'popup__button_disabled',
-  inputErrorClass: 'popup__input_type_error',
-  errorClass: 'popup__error_visible',
+  formSelector: '.popup__content',
+  inputSelector: '.popup__submit-text',
+  submitButtonSelector: '.popup__submit-button',
+  inactiveButtonClass: 'popup__submit-button_type_inactive',
+  inputErrorClass: 'popup__submit-text_type_error',
+  errorClass: 'popup__submit-text-error_type_active',
 };
+
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+  inputElement.classList.add(input.inputErrorClass);
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add(input.errorClass);
+};
+
+const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+  inputElement.classList.remove(input.inputErrorClass);
+  errorElement.classList.remove(input.errorClass);
+  errorElement.textContent = '';
+};
+
+const checkInputValidity = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+};
+
+// eslint-disable-next-line max-len
+const hasInvalidInput = (inputList) => inputList.some((inputElement) => !inputElement.validity.valid);
+
+const toggleButtonState = (inputList, buttonElement) => {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add(input.inactiveButtonClass);
+    buttonElement.disabled = true;
+    // ? linter doesn't like it, but how else can i make button
+    // ? really disabled? not just look like...
+  } else {
+    buttonElement.classList.remove(input.inactiveButtonClass);
+    buttonElement.disabled = false;
+  }
+};
+
+const setEventListeners = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll(input.inputSelector));
+  const buttonElement = formElement.querySelector(input.submitButtonSelector);
+
+  toggleButtonState(inputList, buttonElement);
+
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', () => {
+      checkInputValidity(formElement, inputElement);
+      toggleButtonState(inputList, buttonElement);
+    });
+  });
+};
+
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll(input.formSelector));
+  formList.forEach((formElement) => {
+    formElement.addEventListener('submit', (e) => {
+      e.preventDefault();
+    });
+
+    setEventListeners(formElement);
+  });
+};
+
+enableValidation(input);
